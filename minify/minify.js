e6e19a104105ce4a285e6ed472420e2fd8bce25e
dumpster-TechOps <166152093+dumpster-TechOps@users.noreply.github.com>
2025-07-29 12:58:26 -0700

    Add ESLint configuration and fix lint issues

--- minify/minify.js
+++ minify/minify.js
@@ -34,5 +34,7 @@ async function doMinify() {
 }
 
 console.log("Minifying");
-await doMinify();
-console.log("Minifying Done!");
+(async () => {
+    await doMinify();
+    console.log("Minifying Done!");
+})();
